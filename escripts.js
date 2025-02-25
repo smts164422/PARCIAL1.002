6b0ebd5052b3939341be67e757b09d1519316229
smts164422 <smts164422@ugb.edu.sv>
2025-02-25 13:46:37 -0600

    first commit

---
+++ escripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded"),function(){
+    img1 = document.querySelector("#zanimg");
+    img1.addEventListener(onmouseover=pasa());
+
+    function pasa(){
+        document.querySelector("#zanimg").onmouseover(b)
+    }
+}
